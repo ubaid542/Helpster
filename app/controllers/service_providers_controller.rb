1113b5fbf0d12f82f299e45fd2f5ef3000067d1f
Ubaid <ranaubaid542@gmail.com>
2025-07-15 10:33:31 +0500

    updates

--- app/controllers/service_providers_controller.rb
+++ app/controllers/service_providers_controller.rb
@@ -24,7 +24,12 @@ class ServiceProvidersController < ApplicationController
     {
         "Electrician" => ["Wiring", "Lighting", "Fan Installation"],
         "Plumber" => ["Leak Fixing", "Pipe Installation", "Water Tank Repair"],
-        "Mechanic" => ["Engine Repair", "Oil Change", "Tire Replacement"]
+        "Mechanic" => ["Engine Repair", "Oil Change", "Tire Replacement"],
+        "House Cleaner" => ["car washer", "Carpet Cleaner", "Tire Replacement"],
+        "Carpenter" => ["Engine Repair", "Oil Change", "Tire Replacement"],
+        "Painter" => ["Engine Repair", "Oil Change", "Tire Replacement"],
+        "Gardener" => ["Engine Repair", "Oil Change", "Tire Replacement"]
+        
     }[category] || []
     end
 

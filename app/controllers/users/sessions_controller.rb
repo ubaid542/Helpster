99ecfd3c8b017d5c482fb5dc810eaaa5fa018321
Ubaid <ranaubaid542@gmail.com>
2025-07-16 14:48:04 +0500

    update

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,50 +1,51 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
+
 
   def new
     self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    
+    # Set up necessary variables for the view
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    @resource = resource
+    @resource_name = resource_name
     
     respond_to do |format|
-      format.html { render :new }
-      format.turbo_stream { render :new }
+      format.html { render "users/sessions/new" }
+      format.turbo_stream { render "users/sessions/new" }
     end
   end
 
   def create
     self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    respond_with resource, location: after_sign_in_path_for(resource)
+  rescue => e
+    # Handle failed authentication
+    self.resource = resource_class.new(sign_in_params)
+    resource.errors.add(:base, "Invalid email or password")
     
-    if resource
-      set_flash_message!(:notice, :signed_in)
-      sign_in(resource_name, resource)
-      
-      respond_to do |format|
-        format.html { redirect_to after_sign_in_path_for(resource) }
-        format.turbo_stream { redirect_to after_sign_in_path_for(resource) }
-      end
-    else
-      respond_to do |format|
-        format.html { render :new }
-        format.turbo_stream { render :new }
-      end
+    # Set up necessary variables for the view
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    @resource = resource
+    @resource_name = resource_name
+    
+    respond_to do |format|
+      format.html { render "users/sessions/new" }
+      format.turbo_stream { render "users/sessions/new" }
     end
   end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def sign_in_params
+    params.permit(:email, :password)
+  end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
+
+

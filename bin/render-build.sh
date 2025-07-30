ebfab4f9c45019ae0a5a1d2b72d8a7e002238139
Ubaid <ranaubaid542@gmail.com>
2025-07-30 11:54:02 +0500

    Remove apt-get for Render compatibility

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,13 +2,8 @@
 # exit on error
 set -o errexit
 
-# Install Node and Yarn (for cssbundling-rails)
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash - && \
-apt-get install -y nodejs && \
-npm install --global yarn
-
 bundle install
-yarn install --check-files
+yarn install
+yarn build:css
 bundle exec rails assets:precompile
-bundle exec rails assets:clean
 bundle exec rails db:migrate

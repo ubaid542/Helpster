736123fa7a04ec275d77602170d393daac66e24f
Ubaid <ranaubaid542@gmail.com>
2025-07-30 11:20:20 +0500

    Fix build: add Node.js and Yarn for cssbundling-rails

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
+# Install Node and Yarn (for cssbundling-rails)
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash - && \
+apt-get install -y nodejs && \
+npm install --global yarn
+
 bundle install
+yarn install --check-files
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
